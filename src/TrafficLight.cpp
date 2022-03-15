a34a49587097ed9a101c5d5a962bddeb96118b79
Ujjwal Dalmia <ujjwal.dalmia@elroyair.com>
2022-03-15 16:18:04 -0700

    function to cycle through traffic light phases

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -23,36 +23,44 @@ void MessageQueue<T>::send(T &&msg)
 
 /* Implementation of class "TrafficLight" */
 
-/* 
 TrafficLight::TrafficLight()
 {
     _currentPhase = TrafficLightPhase::red;
 }
 
+/* 
 void TrafficLight::waitForGreen()
 {
     // FP.5b : add the implementation of the method waitForGreen, in which an infinite while-loop 
     // runs and repeatedly calls the receive function on the message queue. 
     // Once it receives TrafficLightPhase::green, the method returns.
 }
-
-TrafficLightPhase TrafficLight::getCurrentPhase()
-{
-    return _currentPhase;
-}
+*/
 
 void TrafficLight::simulate()
 {
-    // FP.2b : Finally, the private method „cycleThroughPhases“ should be started in a thread when the public method „simulate“ is called. To do this, use the thread queue in the base class. 
+    // launch cycleThroughPhases in a thread
+    threads.emplace_back(std::thread(&TrafficLight::cycleThroughPhases, this));
 }
 
 // virtual function which is executed in a thread
 void TrafficLight::cycleThroughPhases()
 {
-    // FP.2a : Implement the function with an infinite loop that measures the time between two loop cycles 
-    // and toggles the current phase of the traffic light between red and green and sends an update method 
-    // to the message queue using move semantics. The cycle duration should be a random value between 4 and 6 seconds. 
-    // Also, the while-loop should use std::this_thread::sleep_for to wait 1ms between two cycles. 
-}
+    auto start_time = std::chrono::system_clock::now();
+    std::chrono::duration<double> cycle_time = std::chrono::milliseconds(4000 + 2000 * (rand() / RAND_MAX));
 
-*/
+    // Infinite loop that cycles through red and green lights
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        if ((std::chrono::system_clock::now() - start_time) < cycle_time)
+            continue;
+        if (_currentPhase == TrafficLightPhase::red) {
+            _currentPhase = TrafficLightPhase::green;
+        } else {
+            _currentPhase = TrafficLightPhase::red;
+        }
+        _queue.send(std::move(_currentPhase));
+        cycle_time = std::chrono::milliseconds(4000 + 2000 * (rand() / RAND_MAX));
+        start_time = std::chrono::system_clock::now();
+    }
+}
